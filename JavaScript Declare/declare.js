2134f956d979e1eb07638ebdb5583f05e4912cb8
4dar <adarbars@gmail.com>
2017-10-23 08:47:35 -0500

    i do declare

---
+++ JavaScript Declare/declare.js
@@ -0,0 +1,14 @@
+var numb1 = 5;
+var numb2 = 4.4;
+var numb3 = -23;
+var numb4 = 184;
+console.log("four numbers", numb1, numb2, numb3, numb4);
+
+var string1 = 'yo';
+var string2 = 'codingisamazing';
+var string3 = 'mom?';
+console.log("these are my strings", string1, string2, string3);
+
+var bool1 = true;
+var bool2 = false;
+console.log("true or false?", bool1, bool2);
