a1c532fe761d196a28317735353e78ce2da896f1
4dar <adarbars@gmail.com>
2017-10-30 08:50:21 -0500

    lots of assn updates

--- Random Chance/randomchance.js
+++ Random Chance/randomchance.js
@@ -1,29 +1,81 @@
-function insertCoin(quarters){
-    while(quarters > 0){
-        var winnings = Math.floor(Math.random()*100);
-        if(winnings == 1){
-            var amountWon = Math.random()*50 + 50;
-            return amountWon + quarters;
-        }
-    }
-    return 0;
+function getrandom(){
+    var rand = Math.floor(Math.random()*100);
+    return rand;
 }
 
-function insertCoinBonus(quarters, goal){
-    while(quarters > 0){
-        var winnings = Math.floor(Math.random()*100);
-        if(winnings == 1){
-            var amountWon = Math.floor(Math.random()*50 +50)
-            if(amountWon + quarters > goal){
-                return amountWon + quarters}
-                else{
-                    quarters = quarters + amountWon;
-                    console.log("won:", quarters);
-                }
-            }
-        }
-        console.log("lossed");
-        return 0;
+function playslot(){
+    var x = document.pageform.slotnum.value;
+    var r = getrandom();
+    var w = -1;
+    if(x == r){
+       w = getwinning();
     }
+    console.log('user: '+x);
+    console.log('machine: '+r);
+    console.log('coins: '+ w);
+    
+}
+function getwinning(){
+    var wins = Math.floor(Math.random()*50+50);
+    return wins;
+}
 
-insertCoinBonus(100, 200);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function insertCoin(quarters){
+//     while(quarters > 0){
+//         var winnings = Math.floor(Math.random()*100);
+//         if(winnings == 1){
+//             var amountWon = Math.random()*50 + 50;
+//             return amountWon + quarters;
+//         }
+//     }
+//     return 0;
+// }
+
+// // function insertCoinBonus(quarters, goal){
+// //     while(quarters > 0){
+// //         var winnings = Math.floor(Math.random()*100);
+// //         if(winnings == 1){
+// //             var amountWon = Math.floor(Math.random()*50 +50)
+// //             if(amountWon + quarters > goal){
+// //                 return amountWon + quarters}
+// //                 else{
+// //                     quarters = quarters + amountWon;
+// //                     console.log("won:", quarters);
+// //                 }
+// //             }
+// //         }
+// //         console.log("lossed");
+// //         return 0;
+// //     }
+
+// // insertCoinBonus(100, 200);
--- Students and Instructors/stuandinst.js
+++ Students and Instructors/stuandinst.js
@@ -1,35 +1,34 @@
-var students = [ 
-    {first_name:  'Michael', last_name : 'Jordan'},
-    {first_name : 'John', last_name : 'Rosales'},
-    {first_name : 'Mark', last_name : 'Guillen'},
-    {first_name : 'KB', last_name : 'Tonel'}
-]
+// var students = [ 
+//     {first_name:  'Michael', last_name : 'Jordan'},
+//     {first_name : 'John', last_name : 'Rosales'},
+//     {first_name : 'Mark', last_name : 'Guillen'},
+//     {first_name : 'KB', last_name : 'Tonel'}
+// ]
 
-function Student(){
-    for(var i in students){
-        console.log(students[i].first_name + " " + students[i].last_name);
-    }
-}
-Student(students);
-
-// var users = {
-//     'Students': [ 
-//         {first_name:  'Michael', last_name : 'Jordan'},
-//         {first_name : 'John', last_name : 'Rosales'},
-//         {first_name : 'Mark', last_name : 'Guillen'},
-//         {first_name : 'KB', last_name : 'Tonel'}
-//      ],
-//     'Instructors': [
-//         {first_name : 'Michael', last_name : 'Choi'},
-//         {first_name : 'Martin', last_name : 'Puryear'}
-//      ]
-//     }
-
-// function studentsAndInst(){
-//     var count = 0;
-//     for(var i in users){
-//         console.log(count += 1, users[i]);
+// function Student(){
+//     for(var i in students){
+//         console.log(students[i].first_name + " " + students[i].last_name);
 //     }
 // }
+// Student(students);
 
-// studentsAndInst(users);
+var users = {
+    'Students': [ 
+        {first_name:  'Michael', last_name : 'Jordan'},
+        {first_name : 'John', last_name : 'Rosales'},
+        {first_name : 'Mark', last_name : 'Guillen'},
+        {first_name : 'KB', last_name : 'Tonel'}
+     ],
+    'Instructors': [
+        {first_name : 'Michael', last_name : 'Choi'},
+        {first_name : 'Martin', last_name : 'Puryear'}
+     ]
+    }
+
+function studentsAndInst(){
+    var count = 0;
+    for(var i in users){
+        console.log(users);
+    }
+}
+studentsAndInst(users);
